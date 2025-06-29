614cbaa8c1a3466fa729b566fe780fe9df1d2711
kobtmk123 <215819376+kobtmk123@users.noreply.github.com>
2025-06-29 12:33:51 +0700

    bruh

--- src/main/java/vn/tutienhi/models/Realm.java
+++ src/main/java/vn/tutienhi/models/Realm.java
@@ -7,13 +7,52 @@ public class Realm {
     private final String id;
     private final String displayName;
     private final double maxLinhKhi;
-    // ... các trường khác
+    private final double linhKhiPerTick;
+    private final double lightningDamage;
+    private final List<String> permanentEffects;
+    private final double bonusHealth;
+    private final double bonusDamage;
 
-    public Realm(String id, String displayName, double maxLinhKhi, ...) {
+    public Realm(String id, String displayName, double maxLinhKhi, double linhKhiPerTick, double lightningDamage, List<String> permanentEffects, double bonusHealth, double bonusDamage) {
         this.id = id;
         this.displayName = ChatUtil.colorize(displayName);
-        // ...
+        this.maxLinhKhi = maxLinhKhi;
+        this.linhKhiPerTick = linhKhiPerTick;
+        this.lightningDamage = lightningDamage;
+        this.permanentEffects = permanentEffects;
+        this.bonusHealth = bonusHealth;
+        this.bonusDamage = bonusDamage;
     }
 
-    // ... các getters
+    public String getId() {
+        return id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public double getMaxLinhKhi() {
+        return maxLinhKhi;
+    }
+
+    public double getLinhKhiPerTick() {
+        return linhKhiPerTick;
+    }
+
+    public double getLightningDamage() {
+        return lightningDamage;
+    }
+
+    public List<String> getPermanentEffects() {
+        return permanentEffects;
+    }
+
+    public double getBonusHealth() {
+        return bonusHealth;
+    }
+
+    public double getBonusDamage() {
+        return bonusDamage;
+    }
 }
